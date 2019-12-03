e49af707a24d647074cd75407f906c17a4c9fa08
Filip Grudzien <filip7grudzien@gmail.com>
2019-12-03 20:59:57 +0100

    initial component created, partial project structure created, bootswatch theme applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskRowComponent } from './home/components/task-row/task-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskRowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/components/task-row/task-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-row',
+  templateUrl: './task-row.component.html',
+  styleUrls: ['./task-row.component.css']
+})
+export class TaskRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
